add6e8367eecea2de832cf40cabdeeb821c94970
pawsley <ilhamirwansyah12@gmail.com>
2024-02-17 03:24:15 +0700

    config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:9090/dhtech/';
+// $config['base_url'] = 'http://localhost:9090/dhtech/';
+$config['base_url'] = 'https://live.akira.id/dhtech/dev-dhtech/';
 
 /*
 |--------------------------------------------------------------------------
