a1f61caa507e2b0db8ac850f82bb99bd67144409
pawsley <ilhamirwansyah12@gmail.com>
2024-02-17 04:58:52 +0700

    last update

---
+++ application/models/BarangTerima_model.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class BarangTerima_model extends CI_Model {
+
+}
+
+/* End of file BarangTerima_model.php */
+/* Location: ./application/models/BarangTerima_model.php */
