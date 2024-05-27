6ae0f1e63cf6f3288ceb0b5dcd0f767be3c6fe26
Pawsley <ilhamirwansyah12@gmail.com>
2024-05-27 13:34:07 +0700

    update model orderin

--- application/models/PenOrderIn_model.php
+++ application/models/PenOrderIn_model.php
@@ -29,7 +29,7 @@ class PenOrderIn_model extends CI_Model {
   }
   public function cancel($inv, $data) {
     $this->db->where('kode_penjualan', $inv);
-    $this->db->update('tb_penjualan', $data);
+    $this->db->update('tb_detail_penjualan', $data);
   }
   public function stok($idk, $data) {
     $this->db->where('id_keluar', $idk);
